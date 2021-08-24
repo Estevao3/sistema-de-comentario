95f2308fc18d7984f50dcc294842b1841e4fc963
Estevao <estevaom404@gmail.com>
2021-08-24 11:44:20 -0300

    Criando a base visual do sistema de comentário

---
+++ index.php
@@ -0,0 +1,9 @@
+<fieldset>
+	<form>
+		Nome:<br>
+		<input type="text" name="nome"><br><br>
+		Mensagem:<br>
+		<textarea name="mensagem"></textarea><br><br>
+		<input type="submit" value="Enviar Mensagem">
+	</form>
+</fieldset>
